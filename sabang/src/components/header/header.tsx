219644ceb604502e3dbac19da2155e6fffa8faed
Aidris01 <valitstroc@gmail.com>
2023-08-07 08:08:28 +0000

    commit

--- sabang/src/components/header/header.tsx
+++ sabang/src/components/header/header.tsx
@@ -3,11 +3,18 @@ import user from './img/user.png';
 import logout from './img/logout.png';
 import './header.css';
 
+function Logout() {
+  return(
+    alert("Logout")
+  )
+}
+
 function Header() {
   return (
     <div className='header'>
       <img className='user-img' src={user} alt="user.jpg" />
       <h5>Welcome, (Username)</h5>
+      <button onClick={Logout}>Sign Out</button>
       <img className='logout-img' src={logout} alt='logout.jpg' />
     </div>
   )
--- sabang/src/components/pages/Dashboard/Dashboard.tsx
+++ sabang/src/components/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,82 @@
+import { Card, Space, Statistic, Table, Typography } from 'antd';
 import React from 'react';
 import '../../pages/style/style.css'
 
 function Dashboard() {
   return (
     <div className='content'>
+      <Typography.Title level={4}>Dashboard</Typography.Title>
       <div className='dashboard-page'>
-        Dashboard
+        <Space direction='horizontal'>
+          <DashboardCard title={"All Purchases (Liter)"} value={1000} />
+          <DashboardCard title={"All Productions (Kg)"} value={2000} />
+          <DashboardCard title={"ICS Checked (Place)"} value={0} />
+          <DashboardCard title={"All Payments (Rp)"} value={3000} />
+        </Space>
+        <Space>
+          <Typography.Title level={4}>Purchase<Purchase /></Typography.Title>
+          <Typography.Title level={4}>Production<Production /></Typography.Title>
+          <Typography.Title level={4}>ICS Progress<ICS /></Typography.Title>
+        </Space>
       </div>
     </div>
+  )
+}
+
+function DashboardCard({ title, value }: {title: string; value: number; }) {
+  return (
+    <Card>
+      <Space direction='horizontal'>
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  )
+}
+
+function Purchase() {
+  return (
+    <Table
+    columns={[
+      {
+        title: "Day",
+        dataIndex: "day"
+      },
+      {
+        title: "Nira Total",
+        dataIndex: "niraTotal"
+      }
+    ]}></Table>
+  )
+}
+function Production() {
+  return (
+    <Table
+    columns={[
+      {
+        title: "Day",
+        dataIndex: "day"
+      },
+      {
+        title: "Weight (Kg)",
+        dataIndex: "weight"
+      }
+    ]}></Table>
+  )
+}
+
+function ICS() {
+  return (
+    <Table
+    columns={[
+      {
+        title: "Day",
+        dataIndex: "day"
+      },
+      {
+        title: "Checked",
+        dataIndex: "checked"
+      }
+    ]}></Table>
   )
 }
 
