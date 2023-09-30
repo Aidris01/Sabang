230deb2f6bfd47cde9471f8e6917ea95795c0eb1
Aidris01 <valitstroc@gmail.com>
2023-09-30 04:05:01 +0000

    DetailUser (in progress)

--- sabang/src/components/pages/UserManagement/ListUser/DetailUser.tsx
+++ sabang/src/components/pages/UserManagement/ListUser/DetailUser.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { Button, Space, Typography } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Button, Space, Typography, Descriptions, DescriptionsProps } from 'antd'
 import '../../style/style.css'
 import { useNavigate, useParams } from 'react-router-dom';
+import axios from '../../../api/axios';
+
+interface UserData {
+    name: string,
+    nik: string,
+    phone: string,
+    address: string,
+    email: string,
+    bankName: string,
+    accName: string,
+    accNumber: string,
+    villageId: string
+}
 
 function DetailUser() {
+    const token = localStorage.getItem('token')
     const { userId } = useParams<Record<string, string>>();
+    const [userData, setUserData] = useState<UserData>(
+        {
+            name: '',
+            nik: '',
+            phone: '',
+            address: '',
+            email: '',
+            bankName: '',
+            accName: '',
+            accNumber: '',
+            villageId: ''
+        }
+    )
+    useEffect(() => {
+        axios.get(`/users/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            setUserData(response.data)
+        }).catch((error) => {
+            console.error('Error Occured: ',error)
+        })
+    }, [userId])
     const navigate = useNavigate()
     const back = () => {
         navigate('/ListUser')
