4833c22c8a33c38179e3817e370161fabbf76608
Aidris01 <valitstroc@gmail.com>
2023-10-02 03:48:34 +0000

    Pembuatan UserDetail (done)

--- sabang/src/components/pages/UserManagement/ListUser/DetailUser.tsx
+++ sabang/src/components/pages/UserManagement/ListUser/DetailUser.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Space, Typography, Descriptions, DescriptionsProps } from 'antd'
+import { Button, Col, Descriptions, Row, Space, Typography } from 'antd'
 import '../../style/style.css'
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from '../../../api/axios';
@@ -13,12 +13,13 @@ interface UserData {
     bankName: string,
     accName: string,
     accNumber: string,
-    villageId: string
+    villageId: string,
+    roles: string[]
 }
-
 function DetailUser() {
     const token = localStorage.getItem('token')
     const { userId } = useParams<Record<string, string>>();
+    const [villageName, setVillageName] = useState('');
     const [userData, setUserData] = useState<UserData>(
         {
             name: '',
@@ -29,7 +30,8 @@ function DetailUser() {
             bankName: '',
             accName: '',
             accNumber: '',
-            villageId: ''
+            villageId: '',
+            roles: []
         }
     )
     useEffect(() => {
@@ -38,11 +40,20 @@ function DetailUser() {
                 Authorization: `Bearer ${token}`
             }
         }).then((response) => {
-            setUserData(response.data)
+            setUserData(response.data);
+            axios.get(`/villages/${response.data.villageId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((villageResponse) => {
+                setVillageName(villageResponse.data.name);
+            }).catch((villageError) => {
+                console.error('Error Occurred while fetching village name: ', villageError);
+            });
         }).catch((error) => {
-            console.error('Error Occured: ',error)
-        })
-    }, [userId])
+            console.error('Error Occurred: ', error);
+        });
+    }, [userId]);
     const navigate = useNavigate()
     const back = () => {
         navigate('/ListUser')
@@ -58,6 +69,20 @@ function DetailUser() {
                     <Button className='back-btn' danger onClick={back}>Back</Button>
                     <Button className='edit-btn' type='primary' onClick={edit}>Edit</Button>
                 </Space>
+                <div className="user-info">
+                    <Descriptions title='User Detail' layout='vertical'>
+                        <Descriptions.Item label='Name'>{userData.name}</Descriptions.Item>
+                        <Descriptions.Item label='NIK'>{userData.nik}</Descriptions.Item>
+                        <Descriptions.Item label='Email'>{userData.email}</Descriptions.Item>
+                        <Descriptions.Item label='Phone'>{userData.phone}</Descriptions.Item>
+                        <Descriptions.Item label='Address'>{userData.address}</Descriptions.Item>
+                        <Descriptions.Item label='Village ID'>{villageName}</Descriptions.Item>
+                        <Descriptions.Item label='Bank Name'>{userData.bankName}</Descriptions.Item>
+                        <Descriptions.Item label='Acc Name'>{userData.accName}</Descriptions.Item>
+                        <Descriptions.Item label='Acc Number'>{userData.accNumber}</Descriptions.Item>
+                        <Descriptions.Item label='Roles'>{userData.roles}</Descriptions.Item>
+                    </Descriptions>
+                </div>
             </div>
         </div>
     )
