1486c07f6bb9160eb4a7d3b611c111700e488df0
Aidris01 <valitstroc@gmail.com>
2023-10-11 09:54:57 +0000

    Revisi userBulk

--- sabang/src/components/pages/UserManagement/ListUser/CreateUserBulk.tsx
+++ sabang/src/components/pages/UserManagement/ListUser/CreateUserBulk.tsx
@@ -1,49 +1,66 @@
-import { CloseOutlined, SaveOutlined } from '@ant-design/icons'
-import { Button, Input, Space, Typography } from 'antd'
-import React, { useState, ChangeEvent, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import '../../style/style.css'
+import { CloseOutlined, SaveOutlined, UploadOutlined } from '@ant-design/icons';
+import { Button, Form, Space, Typography, Upload } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../style/style.css';
 
 function CreateUserBulk() {
   useEffect(() => {
-    document.title = 'Sabang | Create Bulk'
-  }, [])
-  const navigate = useNavigate()
-  const [file, setFile] = useState<File | null>(null)
+    document.title = 'Sabang | Create Bulk';
+  }, []);
+  const navigate = useNavigate();
+  const [file, setFile] = useState<File | null>(null);
 
   const createBulk = () => {
-    navigate("/ListUser")
-  }
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const selectedFile = e.target.files[0];
-      setFile(selectedFile); // Menyimpan file yang dipilih dalam state
+    navigate('/ListUser');
+  };
+
+  const onFinish = (values: any) => {
+    // Kirim data atau lakukan tindakan yang sesuai dengan data yang dikumpulkan dari formulir.
+    console.log('Form values:', values);
+    console.log('Selected file:', file);
+  };
+
+  const handleFileChange = (info: any) => {
+    if (info.fileList.length > 0) {
+      const selectedFile = info.fileList[0].originFileObj;
+      setFile(selectedFile);
     }
-  }
+  };
 
   return (
     <div className='content'>
       <Typography.Title level={4}>Create User Bulk</Typography.Title>
       <div className='main-container'>
-        <Input
-          className='input-container'
-          style={{ marginLeft: 15, marginRight: 10, width: 950 }}
-          type='file'
-          accept='.csv'
-          onChange={handleFileChange}
-          />
-        <div className="button-container">
-          <Space size={10}>
-            <Button className='save-btn' type='primary' htmlType='submit' icon={<SaveOutlined />}>
-              Save
-            </Button>
-            <Button className='cancel-btn' danger onClick={createBulk} icon={<CloseOutlined />}>
-              Cancel
-            </Button>
-          </Space>
-        </div>
+        <Form name="createUserBulk" onFinish={onFinish} hideRequiredMark>
+          <Form.Item name="file" valuePropName="fileList" getValueFromEvent={(e) => e.fileList}>
+            <div className="input-upload">
+              <Upload.Dragger
+                className='input-container'
+                accept='.csv'
+                name="file"
+                onChange={handleFileChange}
+                style={{borderRadius: 1, border: 'dashed 1px rgba(46, 46, 46, 0.5)'}}>
+                <UploadOutlined />
+                <br />
+                Upload File .csv
+              </Upload.Dragger>
+            </div>
+          </Form.Item>
+          <div className="button-container">
+            <Space size={10}>
+              <Button className='save-btn' type='primary' htmlType='submit' icon={<SaveOutlined />}>
+                Save
+              </Button>
+              <Button className='cancel-btn' danger onClick={createBulk} icon={<CloseOutlined />}>
+                Cancel
+              </Button>
+            </Space>
+          </div>
+        </Form>
       </div>
     </div>
-  )
+  );
 }
-export default CreateUserBulk
+
+export default CreateUserBulk;
