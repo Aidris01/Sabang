08fcbf61eab66b6f539275822aa3439a3e7e2b3e
Aidris01 <valitstroc@gmail.com>
2023-10-11 02:48:58 +0000

    Menambah api cadangan

--- sabang/src/components/api/axios.tsx
+++ sabang/src/components/api/axios.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://192.168.102.137:3001'
+    baseURL: 'http://192.168.102.10:3001'
 });
+
+// baseURL asli : http://192.168.102.137:3001
+// baseURL backup : http://192.168.102.10:3001
