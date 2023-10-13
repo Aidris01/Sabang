1d3003ef405b2eb0818127ba84eb204cb9e24382
Aidris01 <valitstroc@gmail.com>
2023-10-13 07:06:36 +0000

    Perbaikan EditPurchase

--- sabang/src/components/pages/PurchaseICS/Purchase/EditPurchase.tsx
+++ sabang/src/components/pages/PurchaseICS/Purchase/EditPurchase.tsx
@@ -1,4 +1,5 @@
-import { Form, Input, message, Select, Spin, Typography } from 'antd'
+import { CloseOutlined, SaveOutlined } from '@ant-design/icons'
+import { Button, Form, Input, message, Select, Space, Spin, Typography } from 'antd'
 import { useForm } from 'antd/es/form/Form'
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -10,7 +11,10 @@ interface PurchaseData {
     purchaserId: number,
     ph: number,
     sugarLevel: number,
-    volume: number
+    volume: number,
+    lat: number,
+    lng: number,
+    amount: number
 }
 interface Penyadap {
     id: number,
@@ -29,9 +33,15 @@ function EditPurchase() {
         purchaseId: form.getFieldValue('purchaserId') || 0,
         sugarLevel: form.getFieldValue('sugarLevel') || 0,
         volume: form.getFieldValue('volume') || 0,
+        amount: form.getFieldValue('amount') || 0,
+        lat: form.getFieldValue('lat') || 0,
+        lng: form.getFieldValue('lng') || 0,
         ph: form.getFieldValue('ph') || 0
     }
     const navigate = useNavigate()
+    const handleCancel = () => {
+        navigate('/Purchase')
+    }
     const [penyadap, setPenyadap] = useState<Penyadap[]>([])
     const [purchase, setPurchase] = useState<PurchaseData>(
         {
@@ -39,6 +49,9 @@ function EditPurchase() {
             purchaserId: 0,
             sugarLevel: 0,
             volume: 0,
+            amount: 0,
+            lat: 0,
+            lng: 0,
             ph: 0
         }
     )
@@ -72,6 +85,19 @@ function EditPurchase() {
             setLoading(false)
         })
     }, [])
+    const onFinish = (values: any) => {
+        axios.patch(`/purchases/${purchaseId}`, form.getFieldsValue(), {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            message.success('Puchase Updated')
+            navigate('/Purchase')
+        }).catch((error) => {
+            message.error('Error Ocured')
+            console.error('Error Ocured: ', error)
+        })
+    }
     return (
         <div className='content'>
             <Typography.Title level={4}>Edit Purchase - {purchaseId}</Typography.Title>
@@ -83,6 +109,7 @@ function EditPurchase() {
                         labelCol={{ span: 2 }}
                         wrapperCol={{ span: 22 }}
                         hideRequiredMark
+                        onFinish={onFinish}
                         initialValues={initialValues}>
                         <Form.Item
                             label='Penyadap'
@@ -118,6 +145,31 @@ function EditPurchase() {
                             rules={[{ required: true, message: 'Please input the ph!' }]}>
                             <Input />
                         </Form.Item>
+                        <Form.Item
+                            label='Lat'
+                            name='lat'>
+                            <Input disabled />
+                        </Form.Item>
+                        <Form.Item
+                            label='Long'
+                            name='lng'>
+                            <Input disabled />
+                        </Form.Item>
+                        <Form.Item
+                            label='Amount'
+                            name='amount'>
+                            <Input disabled />
+                        </Form.Item>
+                        <div className="button-container">
+                            <Space>
+                                <Button className='save-btn' type='primary' htmlType='submit' icon={<SaveOutlined />}>
+                                    Save
+                                </Button>
+                                <Button className='cancel-btn' danger onClick={handleCancel} icon={<CloseOutlined />}>
+                                    Cancel
+                                </Button>
+                            </Space>
+                        </div>
                     </Form>
                 </Spin>
             </div>
