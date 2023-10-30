2184a0be0e3ca85bcaa8441a7657bd3df048bd87
Aidris01 <valitstroc@gmail.com>
2023-10-30 09:28:08 +0000

    update API

--- sabang/src/components/api/axios.tsx
+++ sabang/src/components/api/axios.tsx
@@ -4,5 +4,5 @@ export default axios.create({
     baseURL: 'http://192.168.102.182:3001'
 });
 
-// baseURL asli : http://192.168.102.137:3001
+// baseURL asli : http://192.168.102.182:3001
 // baseURL backup : http://192.168.102.10:3001
