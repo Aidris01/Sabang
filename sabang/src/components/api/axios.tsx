beb6a11419d303976d18baad80d248f421bdf0f1
valits <valitstroc@gmail.com>
2023-12-29 08:04:46 +0000

    build

--- sabang/src/components/api/axios.tsx
+++ sabang/src/components/api/axios.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://api-sabang.xyz.co.id/'
+    baseURL: 'http://192.168.102.151:3001'
 });
 
 
 // baseURL asli : http://192.168.102.182:3001
 // baseURL backup : http://192.168.102.10:3001
+// baseUrl sekarang : https://api-sabang.xyz.co.id/
